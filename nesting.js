5d676c120043cacedc01e3a4fdc94a8aefbe61e1
Noal Brown <noalbrown1@gmail.com>
2020-07-10 21:54:52 -0500

    Day 5 Afternoon

--- nesting.js
+++ nesting.js
@@ -80,7 +80,16 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
-
+function removeDuplicates() {
+  for (let i = 0; i < workplaceAccidents.length; i++) {
+    for (let j = workplaceAccidents.length - 1; i > 0; i--) {
+      if (workplaceAccidents[i] === workplaceAccidents[j]) {
+        workplaceAccidents.splice(j, 1)
+      }
+    }
+  }
+  return workplaceAccidents
+}
 
 
 ////////// PROBLEM 3 //////////
